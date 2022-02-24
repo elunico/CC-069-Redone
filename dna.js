c127fb23439b8bc25ca1e1a2f36a2671dddc55d3
Tom <elunico@users.noreply.github.com>
2022-02-24 16:08:34 -0500

    Fixed an issue with missing genes

--- dna.js
+++ dna.js
@@ -17,53 +17,54 @@ const reproductionDesire = "reproduction-desire";
 const ageOfMaturity = "age-of-maturity";
 const adultSize = "adult-size";
 const maxSpeed = "max-speed";
-
-const helpPerception = "help-perception";
+const maliceChance = 'malice-chance';
 
 // Used for loading old format of DNA of vehicles
 function maxForGene(number) {
   return {
-    0: 2, 1: 2, 2: 100, 3: 100, 4: 110, 5: 1, 6: 2, 7: 1, 8: 2, 9: 6 * 60, 10: 7, 11: 8,
+    0: 2, 1: 2, 2: 100, 3: 100, 4: 110, 5: 1, 6: 2, 7: 1, 8: 2, 9: 6 * 60, 10: 7, 11: 8, 12: 0.05
   }[number];
 }
 
 function minForGene(number) {
   return {
-    0: -2, 1: -2, 2: 0, 3: 0, 4: 0, 5: 0, 6: -2, 7: 0, 8: -2, 9: 2 * 60, 10: 3, 11: 3,
+    0: -2, 1: -2, 2: 0, 3: 0, 4: 0, 5: 0, 6: -2, 7: 0, 8: -2, 9: 2 * 60, 10: 3, 11: 3, 12: 0
   }[number];
 }
 
 function nameForGene(number) {
   return {
-    0:  foodDesire,
-    1:  poisonDesire,
-    2:  foodPerception,
-    3:  poisonPerception,
-    4:  othersPerception,
-    5:  donationChance,
-    6:  helpDesire,
-    7:  pityChance,
-    8:  reproductionDesire,
-    9:  ageOfMaturity,
+    0: foodDesire,
+    1: poisonDesire,
+    2: foodPerception,
+    3: poisonPerception,
+    4: othersPerception,
+    5: donationChance,
+    6: helpDesire,
+    7: pityChance,
+    8: reproductionDesire,
+    9: ageOfMaturity,
     10: adultSize,
     11: maxSpeed,
+    12: maliceChance,
   }[number];
 }
 
 function geneForName(name) {
   return {
-    [foodDesire]:         0,
-    [poisonDesire]:       1,
-    [foodPerception]:     2,
-    [poisonPerception]:   3,
-    [othersPerception]:   4,
-    [donationChance]:     5,
-    [helpDesire]:         6,
-    [pityChance]:         7,
+    [foodDesire]: 0,
+    [poisonDesire]: 1,
+    [foodPerception]: 2,
+    [poisonPerception]: 3,
+    [othersPerception]: 4,
+    [donationChance]: 5,
+    [helpDesire]: 6,
+    [pityChance]: 7,
     [reproductionDesire]: 8,
-    [ageOfMaturity]:      9,
-    [adultSize]:          10,
-    [maxSpeed]:           11,
+    [ageOfMaturity]: 9,
+    [adultSize]: 10,
+    [maxSpeed]: 11,
+    [maliceChance]: 12,
   }[name];
 }
 
@@ -86,6 +87,10 @@ class DNA {
   }
 
   getGene(name) {
+    return this.getGeneObject(name).probability;
+  }
+
+  getGeneObject(name) {
     let gene = this.genes[name];
     if (!gene || !gene.probability || isNaN(gene.probability)) {
       let num = geneForName(name);
@@ -94,7 +99,7 @@ class DNA {
       let value = ((geneMax - geneMin) / 2) + geneMin;
       this.genes[name] = new Gene(name, value, geneMin, geneMax);
     }
-    return this.genes[name].probability;
+    return this.genes[name];
   }
 
   crossover(other) {
@@ -108,12 +113,12 @@ class DNA {
 
     for (let i = 0; i < stop; i++) {
       let name = geneNames[i];
-      dna.addGene(first.genes[name].clone());
+      dna.addGene(first.getGeneObject(name).clone());
     }
 
     for (let i = stop; i < geneNames.length; i++) {
       let name = geneNames[i];
-      dna.addGene(second.genes[name].clone());
+      dna.addGene(second.getGeneObject(name).clone());
     }
     return dna;
   }
